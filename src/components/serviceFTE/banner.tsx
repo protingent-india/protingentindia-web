471b04f1aa94aee9fde591ac65705bfc51ea284f
Kashyap Parmar <kashyap.parmar@exceleurservices.com>
2025-09-08 14:49:44 +0530

    FTE banner made responsive

--- src/components/serviceFTE/banner.tsx
+++ src/components/serviceFTE/banner.tsx
@@ -1,23 +1,26 @@
 "use client"
 import Image from 'next/image';
 import { Icon } from '@/components';
+import { useBreakpoint } from '@/hook';
 
 // --------------------------------------------------------------
 
 const BannerSection = () => {
+    const { lg } = useBreakpoint();
+
     return (
-        <div className='w-full flex flex-col justify-between items-center bg-bg-powder-blue' >
-            <div className='w-full lg:w-2/4 flex justify-end p-4' >
-                <div className='w-full lg:w-[74%] flex flex-col gap-8 ' >
-                    <p className='text-4xl text-left lg:text-[50px] text-primary font-bold lg:leading-[56px]' >
+        <div className='w-full flex flex-col gap-8 lg:gap-0 pt-9 lg:pt-0 lg:flex-row justify-between items-center bg-bg-powder-blue' >
+            <div className='w-full xl:w-2/4 flex justify-end p-4 lg:px-8' >
+                <div className='w-full lap:!w-[80%] 3xl:!w-[74%] flex flex-col gap-8 lg:gap-4 ' >
+                    <p className='text-4xl text-left lg:text-4xl 3xl:!text-[50px] text-primary font-bold lap:!leading-[56px]' >
                         Full-Time Equivalent Services for <span className='text-secondry' > Strategic Success</span>
                     </p>
-                    <p className='text-base lg:text-2xl font-normal text-primary' >
+                    <p className='text-base md:text-xl 3xl:!text-2xl font-normal text-primary' >
                         Ensure seamless operations with skilled professionals tailored <br />
                         to your workflows, timelines, and compliance needs.
                     </p>
-                    {/* <div className='w-[424px] bg-bg-parrot-green flex justify-between items-center gap rounded-full pl-6' >
-                        <p className='text-2xl' >
+                    <div className='w-full sm:w-[424px] lg:!w-[324px] 3xl:!min-w-[424px] bg-bg-parrot-green flex justify-between items-center gap rounded-full pl-6' >
+                        <p className='text-lg 3xl:!text-2xl' >
                             Consult Our Expert Recruiters
                         </p>
                         <div className='rounded-full bg-secondry w-[50px] h-[50px] flex justify-center items-center' >
@@ -25,14 +28,14 @@ const BannerSection = () => {
                                 icon='humbleicons:arrow-right'
                             />
                         </div>
-                    </div> */}
+                    </div>
                 </div>
             </div>
             <div
                 style={{
-                    clipPath: "polygon(18% 0%, 100% 0%, 100% 100%, 18% 100%, 4% 50%)",
+                    clipPath: lg ? "polygon(18% 0%, 100% 0%, 100% 100%, 18% 100%, 4% 50%)" : "",
                 }} // polygon(80% 0%, 20% 0%, 20% 100%, 80% 100%, 100% 50%) : for right arrow shape
-                className='overflow-hidden lg:block'>
+                className='overflow-hidden lap:w-2/4'>
                 <Image
                     src={"/img/serviceFTE/banner.png"}
                     width={950}
